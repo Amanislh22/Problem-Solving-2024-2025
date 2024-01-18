dd360747a58856a0824fc7e4f034bde1c489b5c6
Amani Salah <salah.amani2001@gmail.com>
2024-01-18 19:02:06 +0100

    correction prob1  france ioi

--- Problem Solving Betounsi in C++/France_IOI/Level3/problem1_correction.cpp
+++ Problem Solving Betounsi in C++/France_IOI/Level3/problem1_correction.cpp
@@ -1,3 +1,35 @@
 //
 // Created by amani on 18/01/24.
 //
+#include <string>
+#include <iostream>
+using namespace std;
+int sb(string s){
+    int n=s.size();
+    int res=0;
+    for(int i=0;i<n;i++){
+        res+=s[i]-'A';
+    }
+    return res;
+}
+int d(int n){
+    int res=0;
+    while(n>0){
+        res+=n%10;
+        n/=10;
+    }
+    return res;
+}
+int score(string s){
+    int res=sb(s);
+    while(res>=10){
+        res=d(res);
+    }
+    return res;
+}
+int main(){
+    string s1,s2;
+    cin>>s1>>s2;
+    cout<<score(s1)<<" "<<score(s2);
+    return 0;
+}
