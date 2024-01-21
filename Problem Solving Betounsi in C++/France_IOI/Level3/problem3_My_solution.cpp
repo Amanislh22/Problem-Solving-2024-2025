d0131687b947a43998c7aab4d94092f7397a1d67
Amani Salah <salah.amani2001@gmail.com>
2024-01-21 13:36:57 +0100

    problem 3 france IOI Fréquences d’apparition

--- Problem Solving Betounsi in C++/France_IOI/Level3/problem3_My_solution.cpp
+++ Problem Solving Betounsi in C++/France_IOI/Level3/problem3_My_solution.cpp
@@ -3,8 +3,75 @@
 //
 
 #include <string>
+#include <iostream>
+#include <cstring>
+
 using namespace std;
 
+bool isAlp(char c){
+    if((c <='Z' && c>='A')||(c <='z' && c>='a')){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+string toMin(string s){
+
+    for( int i=0;i<(int)s.size();i++){
+        s[i] = tolower(s[i]);
+    }
+    return s;
+}
+
+float score(string s ){
+
+    // nb lettre in a string
+    int n=0;
+    for( int i=0;i<(int)s.size();i++){
+        if(isAlp(s[i])){
+            n++;
+        }
+    }
+    // nb occ
+    int tab[26];
+    memset(tab,0,sizeof(tab));
+    s= toMin(s);
+    int nb ;
+    for ( int i=0;i<(int)s.size();i++){
+
+        if (isAlp(s[i])) {
+            nb=1;
+            for (int j = i + 1; j < (int) s.size() ; j++) {
+                if (s[i] == s[j]) {
+                    nb++;
+                    tab[s[i] - 'a'] = nb;
+                    s[j] = '*';
+                }
+
+            }
+        } else{
+            continue;
+        }
+
+    }
+    cout.precision(9);
+    for(int i=0;i<26;i++){
+        if(tab[i]!=0){
+        cout<<(double )tab[i]/n<<endl;}
+    }
+    return 0;
+}
+
+
 int main(){
+
+    string s;
+    getline(cin,s);
+    score(s);
+
+
+
+
     return 0;
 }
--- Problem Solving Betounsi in C++/France_IOI/Level3/problem3_correction.cpp
+++ Problem Solving Betounsi in C++/France_IOI/Level3/problem3_correction.cpp
@@ -1,3 +1,42 @@
 //
 // Created by amani on 21/01/24.
 //
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isMaj(char c){
+    return c-'A' >=0 && c-'A'<=25;
+}
+bool isMin(char c){
+    return c-'a' >=0 && c-'a'<=25;
+}
+
+bool isAlphabetic(char c){
+    return isMaj(c)|| isMin(c);
+}
+char transMinus(char c){
+    int d=c-'A';
+    char res=d+'a';
+    return res;
+}
+
+int main(){
+    int occ[26];
+    int nb=0;
+    memset(occ,0,sizeof(occ));
+    string s;
+    getline(cin,s);
+    for (int i=0;i<(int)s.size();i++){
+        if(isAlphabetic(s[i])){
+            nb++;
+            if(isMaj(s[i])){
+                s[i]= transMinus(s[i]);
+            }
+            occ[s[i]-'a']++;
+        }
+    }
+    for(int i=0;i<26;i++){
+        cout<<(double)occ[i]/nb<<endl;
+    }
+    return 0;
+}
